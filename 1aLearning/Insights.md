731950604b53fc97b4679ac9af8ff6b7f7266d0a
Subrat29 <ankubanku29062003@gmail.com>
2024-05-16 09:28:40 +0530

    Evaluate boolean binary tree

---
+++ 1aLearning/Insights.md
@@ -0,0 +1,3 @@
+# Insights
+
+- Whenever a problem requires reversing a sequence, it is worth considering using a stack.
---
+++ 2331. Evaluate Boolean Binary Tree/sol1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// sc:
+// tc:
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool evaluateTree(TreeNode *root)
+{
+    if (root->val == 0 || root->val == 1)
+        return root->val;
+
+    if (root->val == 2)
+        return evaluateTree(root->left) || evaluateTree(root->right);
+
+    return evaluateTree(root->left) && evaluateTree(root->right);
+}
+
+int main()
+{
+    return 0;
+}
