87d6f2e1fac1a01d2f2b51db81503b6d37bef82f
Subrat29 <ankubanku29062003@gmail.com>
2024-05-20 01:14:11 +0530

    distributed coins

---
+++ 979. Distribute Coins in Binary Tree/sol1.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// sc: h
+// tc: n
+
+int solve(TreeNode *root, TreeNode *parent)
+{
+    if (!root)
+        return 0;
+
+    int moves = solve(root->left, root) + solve(root->right, root);
+    int coins = root->val - 1;
+    if (parent)
+        parent->val += coins;
+    moves += abs(coins);
+    return moves;
+}
+
+int distributeCoins(TreeNode *root)
+{
+    return solve(root, nullptr);
+}
+
+int main()
+{
+    return 0;
+}
